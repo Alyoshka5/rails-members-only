15903bc086a8aac8391382abdbd0a898c5ae5dd8
Alyoshka <alyoshkamyshko@gmail.com>
2022-08-03 20:55:46 -0700

    Add secret functionality and styling on index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :posts
+  resources :posts, only: [:new, :create, :index]
   root "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
